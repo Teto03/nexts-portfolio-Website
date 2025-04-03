3a3069b934e6e6b321b61da916aa1538a79f710a
Teto03 <bianchi.teto@gmail.com>
2025-04-03 14:28:45 +0200

    Added contact info

--- app/page.tsx
+++ app/page.tsx
@@ -28,9 +28,10 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={100}
       />
-      <h1 className="py-3.5 px-0.5 z-10 text-1xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        BIANCHI FRANCESCO
-      </h1>
+      <h1 className="py-3.5 px-0.5 z-10 text-lg text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-4xl md:text-6xl whitespace-nowrap bg-clip-text ">
+     BIANCHI FRANCESCO
+    </h1>
+
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">

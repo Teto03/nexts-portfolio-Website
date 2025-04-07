dab7e2e045e193e2e41563b063629bda9f7e0d3c
Teto03 <bianchi.teto@gmail.com>
2025-04-07 14:53:08 +0200

    modified a thing

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I'm building solution's for the need's of tomorrow.
+          Computer Science undergraduate at University of Milano-Bicocca with a deep passion for technology and cybersecurity.
         </h2>
       </div>
     </div>
